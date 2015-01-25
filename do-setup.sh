4d06f67cd3c209655e9b2c51cdab801ef485ec2d
Ivan Kozik <ivan@ludios.org>
2015-01-25 16:32:15 +0000

    Organize

--- do-setup.sh
+++ do-setup.sh
@@ -7,12 +7,18 @@ HUMAN_ADMIN_NEEDS="htop dstat tmux git tig wget nano mtr"
 
 . funcs.sh
 
+### UTC timezone
+
 install-config /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
+
+### update package listings and apt
 
 apt-get update
 # Upgrade apt and OpenSSL early in case there were security holes
 upgrade-y apt libssl1.0.0
+
+### etckeeper
 
 aginir-y git
 aginir-y etckeeper
@@ -24,6 +30,8 @@ fi
 
 etckeeper init || echo "Could not etckeeper init; maybe already initialized?"
 
+### non-ntp time managers
+
 # ntpdate can slightly mess up on the time on boot
 remove-purge-y ntpdate
 
@@ -31,6 +39,8 @@ remove-purge-y ntpdate
 remove-purge-y ntpd
 remove-purge-y adjtimex
 remove-purge-y chrony
+
+### /etc/resolv.conf
 
 # resolvconf = put garbage in my /etc/resolv.conf
 remove-purge-y resolvconf
@@ -41,26 +51,16 @@ install-config /etc/resolv.conf
 chattr +i /etc/resolv.conf
 etckeeper commit "Use Google DNS resolvers" || true
 
+### upgrade and install packages
+
 dist-upgrade-y
 aginir-y openssh-server openntpd unattended-upgrades pollinate molly-guard psmisc zsh $HUMAN_ADMIN_NEEDS
 autoremove-purge-y
 apt-get clean
 
+### zsh
+
 chsh -s /bin/zsh
-
-install-config /etc/openntpd/ntpd.conf
-etckeeper commit "Use more time servers" || true
-service openntpd restart
-
-if ! grep -Pxq 'AllowUsers .*' /etc/ssh/sshd_config; then
-	echo "AllowUsers root" >> /etc/ssh/sshd_config
-fi
-if ! grep -Pxq 'MaxSessions .*' /etc/ssh/sshd_config; then
-	echo "MaxSessions 60" >> /etc/ssh/sshd_config
-fi
-service ssh restart
-
-install-config /etc/apt/apt.conf.d/20auto-upgrades
 
 install-config /etc/zsh/zshrc-cont
 if ! grep -Fxq 'source /etc/zsh/zshrc-cont' /etc/zsh/zshrc; then
@@ -78,6 +78,28 @@ if [ -n "$FIRST_USER" ]; then
 	chsh -s /bin/zsh "$FIRST_USER"
 	su "$FIRST_USER" -c "if [ ! -f ~/.zshrc ]; then touch ~/.zshrc; fi"
 fi
+
+### ntpd
+
+install-config /etc/openntpd/ntpd.conf
+etckeeper commit "Use more time servers" || true
+service openntpd restart
+
+### ssh
+
+if ! grep -Pxq 'AllowUsers .*' /etc/ssh/sshd_config; then
+	echo "AllowUsers root" >> /etc/ssh/sshd_config
+fi
+if ! grep -Pxq 'MaxSessions .*' /etc/ssh/sshd_config; then
+	echo "MaxSessions 60" >> /etc/ssh/sshd_config
+fi
+service ssh restart
+
+### automatic security updates
+
+install-config /etc/apt/apt.conf.d/20auto-upgrades
+
+###
 
 echo
 echo "If anything was upgraded (esp. the kernel), you should reboot now."
