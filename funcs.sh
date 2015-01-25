79992d700da54cf05fbb624c814fecc3d309d89e
Ivan Kozik <ivan@ludios.org>
2015-01-25 12:42:04 +0000

    Don't use -q

--- funcs.sh
+++ funcs.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 install-y() {
-	DEBIAN_FRONTEND=noninteractive apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+	DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
 }
 
 upgrade-y() {
-	DEBIAN_FRONTEND=noninteractive apt-get install --upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+	DEBIAN_FRONTEND=noninteractive apt-get install --upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
 }
 
 dist-upgrade-y() {
-	DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+	DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
 }
 
 autoremove-purge-y() {
-	DEBIAN_FRONTEND=noninteractive apt-get autoremove --purge -q -y "$@"
+	DEBIAN_FRONTEND=noninteractive apt-get autoremove --purge -y "$@"
 }
 
 remove-purge-y() {
-	DEBIAN_FRONTEND=noninteractive apt-get remove --purge -q -y "$@" || echo "Could not remove package(s)"
+	DEBIAN_FRONTEND=noninteractive apt-get remove --purge -y "$@" || echo "Could not remove package(s)"
 }
 
 aginir-y() {
