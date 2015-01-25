056ce031e52f7cd94eb9da6ed6a254b28db12bcd
Ivan Kozik <ivan@ludios.org>
2015-01-25 12:35:03 +0000

    Initial commit

---
+++ basic.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+set -u
+
+HUMAN_ADMIN_NEEDS="htop dstat tmux git tig zsh wget nano mtr"
+
+. funcs.sh
+
+echo "Etc/UTC" > /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt-get update
+# Upgrade apt and OpenSSL early in case there were security holes
+upgrade-y apt libssl1.0.0
+
+aginir-y git
+aginir-y etckeeper
+
+if grep -Fxq 'VCS="bzr"' /etc/etckeeper/etckeeper.conf; then
+	sed -i -r 's,^VCS="bzr",#VCS="bzr",g' /etc/etckeeper/etckeeper.conf
+	sed -i -r 's,^#VCS="git",VCS="git",g' /etc/etckeeper/etckeeper.conf
+fi
+
+# ntpdate can slightly mess up on the time on boot
+remove-purge-y ntpdate
+
+# remove ntpd in case it was installed
+remove-purge-y ntpd
+
+# resolvconf = put garbage in my /etc/resolv.conf
+remove-purge-y resolvconf
+
+install-config /etc/resolv.conf
+# Prevent Ubuntu's networking scripts from overwriting it
+chattr +i /etc/resolv.conf
+etckeeper commit "Use Google DNS resolvers"
+
+dist-upgrade-y
+aginir-y openssh-server openntpd unattended-upgrades pollinate molly-guard $HUMAN_ADMIN_NEEDS
+autoremove-purge-y
+apt-get clean
+
+install-config /etc/openntpd/ntpd.conf
+etckeeper commit "Use more time servers"
+service openntpd restart
+
+# TODO: configure unattended-upgrades
+# TODO: configure openssh-server
---
+++ funcs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+install-y() {
+	DEBIAN_FRONTEND=noninteractive apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+}
+
+upgrade-y() {
+	DEBIAN_FRONTEND=noninteractive apt-get install --upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+}
+
+dist-upgrade-y() {
+	DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+}
+
+autoremove-purge-y() {
+	DEBIAN_FRONTEND=noninteractive apt-get autoremove --purge -q -y "$@"
+}
+
+remove-purge-y() {
+	DEBIAN_FRONTEND=noninteractive apt-get remove --purge -q -y "$@"
+}
+
+aginir-y() {
+	install-y --no-install-recommends "$@"
+}
+
+install-config() {
+	cp -a "files/$1" "$1"
+}
