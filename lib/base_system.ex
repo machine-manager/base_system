3bfb918c8f6f037278c24902134b8457098dc3c7
Ivan Kozik <ivan@ludios.org>
2017-01-13 09:40:50 +0000

    Remove ppa-purge from human_admin_needs

--- lib/base_system.ex
+++ lib/base_system.ex
@@ -79,8 +79,7 @@ defmodule BaseSystem.Configure do
 		# dnsutils       - for dig
 		human_admin_needs = ~w(
 			molly-guard iputils-ping less strace htop dstat tmux git tig wget nano
-			mtr-tiny nethogs iftop lsof software-properties-common ppa-purge pv
-			tree dnsutils whois)
+			mtr-tiny nethogs iftop lsof software-properties-common pv tree dnsutils whois)
 		# If custom-packages is available, assume ubuntils, pinned-git, and ripgrep are desired
 		human_admin_needs = case custom_packages do
 			true  -> ["ubuntils", "pinned-git", "ripgrep"] ++ human_admin_needs
