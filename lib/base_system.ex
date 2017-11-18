8a3c4d5764e0bdd0318e3d203e8e420ddd82b7c2
Ivan Kozik <ivan@ludios.org>
2017-11-18 21:22:28 +0000

    Disable consoleblank on pre-4.12-kernel machines

--- lib/base_system.ex
+++ lib/base_system.ex
@@ -1051,13 +1051,13 @@ defmodule BaseSystem.Configure do
 	# https://github.com/stuffo/scaleway-ubuntukernel/tree/28f17d8231ad114034d8bbc684fc5afb9f902758#install
 	defp boot_units("scaleway_kexec", _),         do: [%SystemdUnitDisabled{name: "kexec.service"},
 	                                                   %SystemdUnitEnabled{name: "scaleway-ubuntu-kernel.service"}]
-	defp boot_units("mbr", _),                    do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor"}]
-	defp boot_units("mbr_bfq", _),                do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor elevator=bfq"}]
-	defp boot_units("uefi",     boot_resolution), do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor",              gfxpayload: boot_resolution}]
-	defp boot_units("uefi_bfq", boot_resolution), do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor elevator=bfq", gfxpayload: boot_resolution}]
-	defp boot_units("ovh_vps", _),                do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor",              cmdline_normal_and_recovery: "console=tty1 console=ttyS0"}]
-	defp boot_units("do_vps", _),                 do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor",              cmdline_normal_and_recovery: "console=tty1 console=ttyS0"}]
-	defp boot_units("do_vps_201711", _),          do: [%Grub{cmdline_normal_only: "apparmor=1 security=apparmor biosdevname=0 net.ifnames=0 console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200 consoleblank=0 systemd.show_status=true"}]
+	defp boot_units("mbr", _),                    do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor"}]
+	defp boot_units("mbr_bfq", _),                do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor elevator=bfq"}]
+	defp boot_units("uefi",     boot_resolution), do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor",              gfxpayload: boot_resolution}]
+	defp boot_units("uefi_bfq", boot_resolution), do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor elevator=bfq", gfxpayload: boot_resolution}]
+	defp boot_units("ovh_vps", _),                do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor",              cmdline_normal_and_recovery: "console=tty1 console=ttyS0"}]
+	defp boot_units("do_vps", _),                 do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor",              cmdline_normal_and_recovery: "console=tty1 console=ttyS0"}]
+	defp boot_units("do_vps_201711", _),          do: [%Grub{cmdline_normal_only: "consoleblank=0 apparmor=1 security=apparmor biosdevname=0 net.ifnames=0 console=tty0 console=ttyS0,115200 earlyprintk=ttyS0,115200 systemd.show_status=true"}]
 
 	defp fstab_unit() do
 		fstab_existing_entries = Fstab.get_entries()
